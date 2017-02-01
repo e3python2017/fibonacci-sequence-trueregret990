027388d80654075530017b677ab0d752824ebfdf
Stephen Cerruti <scerruti@e3civichigh.com>
2017-02-01 14:00:46 -0800

    Initial Version

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+def fibonacci(n):
+    """This is documentation string for function. It'll be available by fibonacci.__doc__()
+    Return a list containing the Fibonacci series up to n."""
+    result = []
+
+    # 1. Create a variable named 'a' and set it to 1
+    # a = 1
+
+    # 2. Create a variable named 'b' and set it to 1
+
+    while a < n:
+        result.append(a)
+
+        # 3. set a variable 'tmp_var' equal to 'b'
+
+        # 4. set a variable 'b' equal to the sum of 'b' and 'a'
+
+        # 5. set the variable 'a' to 'tmp_var'
+    return result
+
+print(fibonacci(100))
