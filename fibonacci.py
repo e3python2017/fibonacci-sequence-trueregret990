be2d7faeee0cb7f1340ba42f8b130f571bdc071d
marnie <marnie.oliver@e3civichigh.com>
2017-02-08 12:32:54 -0800

    first thingy

--- fibonacci.py
+++ fibonacci.py
@@ -4,18 +4,21 @@ def fibonacci(n):
     result = []
 
     # 1. Create a variable named 'a' and set it to 1
-    # a = 1
+    a = 1
 
     # 2. Create a variable named 'b' and set it to 1
-
+    b = 1
     while a < n:
         result.append(a)
 
         # 3. set a variable 'tmp_var' equal to 'b'
 
+        tmp_var = b
         # 4. set a variable 'b' equal to the sum of 'b' and 'a'
-
+        b = b+a
         # 5. set the variable 'a' to 'tmp_var'
+        a = tmp_var
+
     return result
 
 print(fibonacci(100))
